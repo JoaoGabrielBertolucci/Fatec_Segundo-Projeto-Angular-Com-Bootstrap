cc976b707f6fc86e05cded1d629c50479eee8d58
João Gabriel Z.B <71906858+JoaoGabrielBertolucci@users.noreply.github.com>
2023-04-18 12:46:54 -0300

    criação da função

--- ConsumoEnergetico/src/app/consumo/consumo.component.ts
+++ ConsumoEnergetico/src/app/consumo/consumo.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./consumo.component.css']
 })
 export class ConsumoComponent {
+  pessoas!: Number;
+  item1: any;
+  item2: any;
+  item3: any;
 
+  teste(){
+    this.item1 = <HTMLInputElement> document.getElementById("check1");
+    this.item2 = <HTMLInputElement> document.getElementById("check2");
+    this.item3 = <HTMLInputElement> document.getElementById("check3");
+
+    if (this.item1.checked) {
+      this.pessoas = 1;
+    }
+
+    if (this.item2.checked) {
+        this.pessoas = 2;
+    }
+
+     if (this.item3.checked) {
+        this.pessoas = 3;
+    }
+  }
 }
+
